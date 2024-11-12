dd418e341b6fe74f7bb7dc69a02d34462c35989a
sucisriaulia <sucisucisriaulia@gmail.com>
2024-11-12 11:49:16 +0800

    Tugas Praktikum 2,3,4,5,6,7,8,9

---
+++ H071241067/Tugas Praktikum 9/tp9ap2024(2)/tp9ap2024/secretcode.py
@@ -0,0 +1,15 @@
+class SecretCode:
+    # TODO: Something is missing here
+
+    def get_code(self):
+        try:
+            return self.code
+        except Exception as e:
+            print(f"Error getting code: {e}")
+            return None
+
+    def set_code(self, new_code):
+        try:
+            self.code = new_code
+        except Exception as e:
+            print(f"Error setting code: {e}")
